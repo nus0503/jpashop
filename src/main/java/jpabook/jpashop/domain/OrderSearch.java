0e30c81e1445f943105710978b0509f25fadb614
kjs <nus0503@naver.com>
2023-10-30 02:00:18 +0900

    동적쿼리 구현

--- src/main/java/jpabook/jpashop/domain/OrderSearch.java
+++ src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -9,4 +9,8 @@ public class OrderSearch {
 
     private String member; // 회원 이름
     private OrderStatus orderStatus; // 주문 상태[ORDER, CANCEL]
+
+    public String getMemberName() {
+        return member;
+    }
 }
