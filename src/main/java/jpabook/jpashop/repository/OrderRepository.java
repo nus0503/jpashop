b2c3abe18bdaedee9a97b822660e83956f13705d
kjs <nus0503@naver.com>
2023-11-25 04:46:44 +0900

    컬렉션 join fetch를 사용한 예시와 사용할 때 주의점

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -103,4 +103,11 @@ public class OrderRepository {
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery("select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o join o.member m join o.delivery d", OrderSimpleQueryDto.class).getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery("select distinct o from Order o join fetch o.member m join fetch o.delivery d join fetch o.orderItems oi join fetch oi.item i", Order.class).getResultList();
+        // 1 : 다 가 아닌 곳에선 페이징 처리를 join fetch를 사용하면서 페이징 처리해도 상관없지만,
+        // 1 : 다 join fetch에서는 페이징을 하면 절대 안된다. 결과값이 나온다고해도 그건 db결과가 아닌 애플리케이션 메모리상에서 만든 결과이기에 리소스 부하가 걸린다. .setFirstResult().setMaxResults()
+        //또한 컬렉션 페치 조인은 1개만 사용해라. 컬렉션 둘 이상에 페치 조인을 사용하면 안된다. 데이터가 부정합하게 조회될 수 있다. ex) Team(1) : Member(N) : Order(N)
+    }
 }
